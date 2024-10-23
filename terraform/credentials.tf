7274dc99d37b9fd79ec1c5c6f021fffdd1718cb3
r3dact3d <redhatbrady@gmail.com>
2024-10-22 20:54:04 -0500

    change

--- terraform/credentials.tf
+++
@@ -1,7 +0,0 @@
-# Provider file
-
-provider "aws" {
-  access_key = "${var.AWS_ACCESS_KEY}"
-  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
-  region     = "us-east-1"
-}
--- terraform/php_webserver.tf
+++ terraform/php_webserver.tf
@@ -9,6 +9,9 @@ terraform {
     aws = {
       source = "hashicorp/aws"
       version = "~> 4.16"
+      access_key = "${var.AWS_ACCESS_KEY}"
+      secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+      region     = "us-east-1"
     }
   }
   required_version = ">= 1.2.0"
