6c1a4e156af4f76c069c019f546063567df3148f
r3dact3d <redhatbrady@gmail.com>
2024-10-22 21:07:24 -0500

    fix region

--- terraform/php_webserver.tf
+++ terraform/php_webserver.tf
@@ -9,9 +9,7 @@ terraform {
     aws = {
       source = "hashicorp/aws"
       version = "~> 4.16"
-      access_key = "${var.AWS_ACCESS_KEY}"
-      secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
-      region     = "us-east-1"
+      region     = "us-east-2"
     }
   }
   required_version = ">= 1.2.0"
