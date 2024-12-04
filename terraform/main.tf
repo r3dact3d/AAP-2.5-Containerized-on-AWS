57d52827f6d3f5077129a50523bf04617fd4bb7f
r3dact3d <redhatbrady@gmail.com>
2024-12-04 17:06:11 -0600

    rm private key

--- terraform/main.tf
+++ terraform/main.tf
@@ -241,7 +241,6 @@ resource "null_resource" "hostname_update" {
       type        = "ssh"
       host        = aws_instance.aap_instance.public_ip
       user        = "ec2-user"
-      private_key = tls_private_key.my_key.private_key_pem
     }
   }
 }
